f10ffbb622d2851f6f440d9407037ad34ecc6bd3
Matthew-Ielusic <matthew38@optonline.net>
2020-03-24 14:31:58 -0700

    Update HTML parsing to account for the updated dashboard format

--- CovidDashboardScraper/TableRow.cs
+++ CovidDashboardScraper/TableRow.cs
@@ -13,9 +13,17 @@ namespace CovidDashboardScraper
         {
             const int NAME_INDEX = 1;
             const int TOTAL_INDEX = 3;
+            const string STAR_HTML = "&#9733;";
 
             string name = trElement.ChildNodes[NAME_INDEX].InnerText.Trim();
             string rawTotal = trElement.ChildNodes[TOTAL_INDEX].InnerText.Trim();
+
+            // The rows for states start with a little star character that needs to be removed
+            // The html for a star character is "&#9733;
+            if (name.StartsWith(STAR_HTML))
+            {
+                name = name.Substring(STAR_HTML.Length).Trim();
+            }
 
             return new TableRow { Name = name, Confirmed = Int32.Parse(rawTotal, NumberStyles.AllowThousands) };
         }
