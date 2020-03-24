af9dd50b62c38bb4c48c7a790533e47193c0543f
Matthew-Ielusic <matthew38@optonline.net>
2020-03-24 13:49:07 -0700

    Remove dead code

--- CovidDashboardScraper/Program.cs
+++ CovidDashboardScraper/Program.cs
@@ -14,10 +14,12 @@ namespace CovidDashboardScraper
     {
         static HttpClient client;
         const string URL = "https://ncov2019.live/data";
+        private const string FILE_PATH = "../../../historical.csv";
+
         static async Task Main(string[] args)
         {
             Console.WriteLine(DateTime.Now);
-            var fileIOTask = CSVHandler.MakeHandlerAsync("../../../historical.csv");
+            var fileIOTask = CSVHandler.MakeHandlerAsync(FILE_PATH);
             var scrapeTask = ScrapeAsync();
             
             List<TableRow> scrapedData = new List<TableRow>();
@@ -46,62 +48,6 @@ namespace CovidDashboardScraper
             }
         }
 
-        private static async Task FromHTMLFile()
-        {
-            List<TableRow> scrapedData = new List<TableRow>();
-            using (System.IO.StreamReader stream = new System.IO.StreamReader("./response.html"))
-            {
-                Task<String> tData = stream.ReadToEndAsync();
-                Console.WriteLine("reading...");
-                string data = await tData;
-                HtmlDocument x = new HtmlDocument();
-                x.LoadHtml(data);
-                string usaDataTable_xPath = "//table[@id='sortable_table_USA']";
-                HtmlNodeCollection search = x.DocumentNode.SelectNodes(usaDataTable_xPath);
-                if (search.Count != 1)
-                {
-                    Console.WriteLine("Found " + search.Count + "tables??!");
-                }
-                else
-                {
-                    HtmlNode tableBody = search[0].SelectSingleNode("tbody");
-                    foreach (HtmlNode row in tableBody.ChildNodes)
-                    {
-                        if (row.Name.Equals("tr"))
-                        {
-                            TableRow processed = TableRow.Parse_tr_Element(row);
-                            scrapedData.Add(processed);
-                        }
-                    }
-                }
-            }
-        }
-
-        private static async Task<Dictionary<String, Int32>> ParseCSVHeaders(string path)
-        {
-            string header;
-            using (StreamReader reader = new StreamReader(path))
-            {
-                header = await reader.ReadLineAsync();
-            }
-            Console.Out.WriteLine(header);
-            string[] columns = header.Split(", ");
-            Dictionary<String, Int32> output = new Dictionary<string, int>();
-            for(int i = 0; i < columns.Length; i++)
-            {
-                output.Add(columns[i], i);
-            }
-            return output;
-        }
-
-        private static void WriteToFile(string data)
-        {
-            using (System.IO.StreamWriter file =
-             new System.IO.StreamWriter(@"./response.html"))
-            {
-                file.WriteLine(data);
-            }
-        }
 
         private static async Task<string> ScrapeAsync()
         {
